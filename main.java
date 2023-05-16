0ba1265c2dd07ae50641a1777e7df4e4329c13a7
sarahdoi <112553205+sarahdoi@users.noreply.github.com>
2023-05-16 21:01:05 +0300

    Update main.java

--- main.java
+++ main.java
@@ -48,47 +48,7 @@ memory[i]=new Partition(partitionSize,sadd,eadd);
         }//end printing method
 
 
-     public static void report(Partition[] p) { 
-		//int m=10;
-		try {
-        BufferedWriter reportFile = new BufferedWriter(new FileWriter("report.txt") );
-            
-		System.out.println("Your memory partitions details : ");
-			
-			
-	     for (int i=0 ; i<p.length ; i++) { //Loop display each partition details
-         System.out.println("Partion " + (i+1) + " :\n");
-		 reportFile.write("Partion " + (i+1) + " :\n");
-		 
-		 /*
-		 System.out.println("Status : " + partition[i].getStatus() );
-		 reportFile.write("Status : " + partition[i].getStatus()   );
-		 
-		 System.out.println("Size : " + partition[i].getSize()   );
-		 reportFile.write("Size : " + partition[i].getSize()   );
-		 
-		 System.out.println("Starting Address : " +  partition[i].getSadd() );
-		 reportFile.write("Starting Address : " + partition[i].getSadd()    );
-		 
-		 System.out.println("Ending Address : " +  partition[i].getEadd() );
-		 reportFile.write("Ending Address : " + partition[i].getEadd()    );	 
-		 
-		 System.out.println("Process ID : " + partition[i].getPid() );
-		 reportFile.write("Process ID : " +  partition[i].getPid()  );	 
-		 
-		 System.out.println("Internal fragmentation size : " +  partition[i].getInternalFrag() );
-		 reportFile.write("Internal fragmentation size : " +  partition[i].getInternalFrag()   );
-		 */
-        }//end for
-		
-           reportFile.flush();
-           reportFile.close();
-		   
-           }catch( IOException e ) {
-			 e.printStackTrace(); 
-			}//end catch
-			
-	}//end method report
+   
 
         
 public static int first(Partition[] p,int processSize){
@@ -131,6 +91,49 @@ if(!found)
     System.out.println("Process not found!!!!");
 
 } //end method deallocate
+
+
+public static void report(Partition[] p) { 
+    //int m=10;
+    try {
+    BufferedWriter reportFile = new BufferedWriter(new FileWriter("report.txt") );
+        
+    System.out.println("Your memory partitions details : ");
+        
+        
+     for (int i=0 ; i<p.length ; i++) { //Loop to display each partition details
+     System.out.println("Partion " + (i+1) + " :\n");
+     reportFile.write("Partion " + (i+1) + " :\n");
+     
+     /*
+     System.out.println("Status : " + partition[i].getStatus() );
+     reportFile.write("Status : " + partition[i].getStatus()   );
+     
+     System.out.println("Size : " + partition[i].getSize()   );
+     reportFile.write("Size : " + partition[i].getSize()   );
+     
+     System.out.println("Starting Address : " +  partition[i].getSadd() );
+     reportFile.write("Starting Address : " + partition[i].getSadd()    );
+     
+     System.out.println("Ending Address : " +  partition[i].getEadd() );
+     reportFile.write("Ending Address : " + partition[i].getEadd()    );	 
+     
+     System.out.println("Process ID : " + partition[i].getPid() );
+     reportFile.write("Process ID : " +  partition[i].getPid()  );	 
+     
+     System.out.println("Internal fragmentation size : " +  partition[i].getInternalFrag() );
+     reportFile.write("Internal fragmentation size : " +  partition[i].getInternalFrag()   );
+     */
+    }//end for
+    
+       reportFile.flush();
+       reportFile.close();
+       
+       }catch( IOException e ) {
+         e.printStackTrace(); 
+        }//end catch
+        
+}//end method report
 
 }//end class
 
