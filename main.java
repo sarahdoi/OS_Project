5bd8e51803c327be5aaa1e05794b263a01222098
Jana <xjax60@gmail.com>
2023-05-16 17:58:01 +0300

    updatedMain

--- main.java
+++ main.java
@@ -10,7 +10,6 @@ public class main {
 
 
 
-
        printing();
     } 
 
@@ -25,10 +24,46 @@ public class main {
         pname=pname+"]";
         System.out.println(pname);
         }
+}//end main
+
         public static void report(){
+}
+        
+public static int first(Partiton[] p,int processSize){
+for(int i=0; i<Partiton.length;i++){
+p pp=p[i];
+if(pp[i].getStatus.equals("Free")&&pp.getSize()>=processSize){return i;}
+return -1;}//end loop
+}
 
+        public static int Best(Partiton[] p,int processSize){
+int best=-1;
+int smallest=10000000;  //or Integer.max();
+for(int i=0; i<Partiton.length;i++){
+p pp=p[i];
+if(pp.getStatus.equals("Free")&&pp.getSize()>=processSize){
+int frag=pp.getSize()-processSize;
+if(frag<=smallest){
+best=i;
+smallest=frag;}
+}
+}//end loop
+return best;}//end method
 
-        }
+public static void deallocate(String pid, Partiton[] p){
 
+boolean found=false;
+for(Partiton partition: p){
+    if(partition.getPid().equals(pid)){
+        partition.setPid("Null");
+        partition.setStatus("Free");
+        partition.setInternalFrag(-1);
+        found=true;
+    }
+}
+if(!found)
+    System.out.println("Process not found!!!!")
 
 }
+
+
